7ca191893ee5ca571e8f540d146dbb19645b8dc7
David J. Desjardins <monsterunits@gmail.com>
2024-02-25 21:50:24 -0700

    steve

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ export default function Home() {
     >
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
           You are gay
+      </div>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
@@ -28,7 +29,6 @@ export default function Home() {
             />
           </a>
         </div>
-      </div>
     </main>
   );
 }
