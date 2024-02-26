8854963d280f00e9c378735a4bb5c39ddfa4a329
David J. Desjardins <monsterunits@gmail.com>
2024-02-25 22:08:16 -0700

    hoh

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,24 +10,26 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className="z-10 fixed max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex custom-font-size">
-          You are gay
-      </div>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+      <div>
+        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex custom-font-size">
+            You are gay
+        </div>
+        <div className="fixed left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/steven.jpeg"
-              alt="London eye"
-              width={1000}
-              height={1000}
-              priority
-            />
-          </a>
+              className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+              href="https://www.pornhub.com/video/search?search=florida+man"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/images/steven.jpeg"
+                alt="London eye"
+                width={1000}
+                height={1000}
+                priority
+              />
+            </a>
+          </div>
         </div>
     </main>
   );
