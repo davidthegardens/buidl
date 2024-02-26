457c40e26cfb94a961157b91f8dea62396911883
David J. Desjardins <monsterunits@gmail.com>
2024-02-25 22:22:57 -0700

    fur

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,25 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import React, { useState } from 'react';
-
-const inter = Inter({ subsets: ["latin"] });
+import React, { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    const audio = new Audio('/sounds/onopen.mp3');
+    audio.play();
+  }, []);
 
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        background: 'url(/images/furry.jpg) no-repeat center center fixed',
+        backgroundSize: 'cover',
+      }}
     >
-      <div>
-        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex custom-font-size">
-            You are gay
-        </div>
-        <div className="fixed center-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-              className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-              href="https://www.pornhub.com/video/search?search=florida+man"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                src="/images/steven.jpeg"
-                alt="London eye"
-                width={1000}
-                height={1000}
-                priority
-              />
-            </a>
-          </div>
-        </div>
+      <h1 style={{ fontSize: '4rem', color: 'white', textAlign: 'center' }}>
+        I LOVE FURRY PORN
+      </h1>
     </main>
   );
 }
