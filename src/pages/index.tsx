cf34abd83c6d0363636d9e1d2195853816a5036c
David J. Desjardins <monsterunits@gmail.com>
2024-02-25 22:12:25 -0700

    bbb

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex custom-font-size">
             You are gay
         </div>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <div className="fixed center-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
               className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
               href="https://www.pornhub.com/video/search?search=florida+man"
